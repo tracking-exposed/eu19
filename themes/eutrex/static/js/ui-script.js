92b7e7b70b8204dfdbcff5afa85955d0210d90a9
lrnzctld <lrnzctld@gmail.com>
2019-04-24 15:42:07 +0200

    homepage content #1

--- themes/eutrex/static/js/ui-script.js
+++ themes/eutrex/static/js/ui-script.js
@@ -179,3 +179,22 @@ function closeAllSelect(elmnt) {
  then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
 
+/**
+ * smooth scrolling
+ */
+jQuery(document).ready(function($) {
+    $('a[href*=\\#]:not([href=\\#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
